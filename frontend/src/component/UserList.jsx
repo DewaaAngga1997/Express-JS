16d3eeb40af7b7612c59747ae067cddf6b10c8aa
Dewa Angga PC <devaangga53@gmail.com>
2024-03-21 21:59:26 +0800

    setup frontend

---
+++ frontend/src/component/UserList.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function UserList() {
+  return (
+    <div className="columns mt-5 is-centered">
+      <div className="column is-half">
+        <h1>Lorem ipsum dolor sit amet.</h1>
+      </div>
+    </div>
+  );
+}
