d527726a5cb2b3aa5efd6a38eb51c8d56d8b2403
Dewa Angga PC <devaangga53@gmail.com>
2024-03-22 20:43:16 +0800

    install tailwindcss

--- frontend/src/component/UserList.jsx
+++ frontend/src/component/UserList.jsx
@@ -1,11 +1,5 @@
 import React from "react";
 
 export default function UserList() {
-  return (
-    <div className="columns mt-5 is-centered">
-      <div className="column is-half">
-        <h1>Lorem ipsum dolor sit amet.</h1>
-      </div>
-    </div>
-  );
+  return <h1 className="">Hello world!</h1>;
 }
